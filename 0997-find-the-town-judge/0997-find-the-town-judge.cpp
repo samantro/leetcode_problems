53f1df9354d88273b17a546751bceb840baafd99
samantro <66985251+samantro@users.noreply.github.com>
2023-01-23 21:42:41 +0530

    Time: 203 ms (70.20%), Space: 60.8 MB (86.87%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>>& trust) {
+        if(N==1)
+            return N;
+        vector<pair<int,int>> arr(N+1,{0,0});
+        for(int i=0;i<trust.size();i++)
+        {
+            arr[trust[i][0]].first+=1;
+            arr[trust[i][1]].second+=1;
+        }
+        for(int i=0;i<=N;i++)
+            if(arr[i].first==0 && arr[i].second==N-1)
+                return i;
+        return -1;
+    }
+};
