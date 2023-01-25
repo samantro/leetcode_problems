fb55f21ec7da7fce149afc6aa2732c59928b2070
samantro <66985251+samantro@users.noreply.github.com>
2023-01-25 21:46:47 +0530

    Time: 409 ms (5.03%), Space: 93.3 MB (55.12%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& p) {
-        int m=0,n=p.size()-1;;
-        priority_queue<int> q;
-        q.push(p[n]);
+        int m=0,n=p.size()-1;
+        int k=p[n];
         for(int i=n-1;i>=0;i--){
-            m=max(m,q.top()-p[i]);
-            // cout<<q.top()<<' ';
-            q.push(p[i]);
+            m=max(m,k-p[i]);
+            k=max(p[i],k);
         }
         return m;
     }
