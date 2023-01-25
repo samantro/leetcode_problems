f1a4ffced5163fe90b4f486abcadbe2713e81496
samantro <66985251+samantro@users.noreply.github.com>
2023-01-25 09:46:44 +0530

    Time: 6 ms (41.81%), Space: 6.7 MB (18.77%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        map<char,int> m;
+        for(int i=0;i<s.size();i++) m[s[i]]++;
+        int ma=0,res=0,k=0;
+        for(auto i:m){
+            if(i.second%2){
+                k+=i.second-1;
+                ma++;
+            }
+            else res+=i.second;
+        }
+        if(ma==0) return res;
+        return res+k+1;
+    }
+};
