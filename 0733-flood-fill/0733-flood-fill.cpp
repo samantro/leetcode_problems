e33fe292ca9f16a6423e2b9d94feb730f31e540b
samantro <66985251+samantro@users.noreply.github.com>
2023-02-04 16:10:12 +0530

    Time: 11 ms (64.98%), Space: 14.1 MB (37.14%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<int>>& v,vector<vector<int>>& image,int im,int color){
-        v[i][j]=1;
-        image[i][j]=color;
-        if(i>0 && image[i-1][j]==im && !v[i-1][j]) dfs(i-1,j,v,image,im,color);
-        if(j>0 && image[i][j-1]==im && !v[i][j-1]) dfs(i,j-1,v,image,im,color);
-        if(i<image.size()-1 && image[i+1][j]==im && !v[i+1][j]) dfs(i+1,j,v,image,im,color);
-        if(j<image[0].size()-1 && image[i][j+1]==im && !v[i][j+1]) dfs(i,j+1,v,image,im,color);
-        
+    void dfs(int i,int j,vector<vector<int>>& image,int im,int color,int n,int m,vector<vector<int>>& v){
+        if(i<0 || j<0 || i>n || j>m) return ;
+        else if(!v[i][j] && image[i][j]==im){
+            v[i][j]=1;
+            image[i][j]=color;
+            dfs(i-1,j,image,im,color,n,m,v);
+            dfs(i+1,j,image,im,color,n,m,v);
+            dfs(i,j-1,image,im,color,n,m,v);
+            dfs(i,j+1,image,im,color,n,m,v);
+        }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int im=image[sr][sc];
         vector<vector<int>> v(image.size(),vector(image[0].size(),0));
-        dfs(sr,sc,v,image,im,color);
+        dfs(sr,sc,image,im,color,image.size()-1,image[0].size()-1,v);
         return image;
     }
 };
