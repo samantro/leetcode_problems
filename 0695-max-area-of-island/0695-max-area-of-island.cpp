552e51beb120ccb4174ddda10c4d80f49c57ddeb
samantro <66985251+samantro@users.noreply.github.com>
2023-02-05 20:35:20 +0530

    Time: 21 ms (69.82%), Space: 23.1 MB (92.06%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int solve(int i,int j,int n,int m,vector<vector<int>>& g,vector<vector<bool>>& vis){
+    int solve(int i,int j,int n,int m,vector<vector<int>>& g){
         if(i<0 || j<0 || i==n || j==m) return 0;
-        if(!vis[i][j] && g[i][j]){
-            vis[i][j]=true;
-            return solve(i-1,j,n,m,g,vis)+solve(i,j-1,n,m,g,vis)+solve(i+1,j,n,m,g,vis)+solve(i,j+1,n,m,g,vis)+1;
+        if(g[i][j]){
+            g[i][j]=0;
+            return solve(i-1,j,n,m,g)+solve(i,j-           1,n,m,g)+solve(i+1,j,n,m,g)+solve(i,j+1,n,m,g)+1;
         }
         return 0;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        vector<vector<bool>> vis(grid.size(),vector<bool>(grid[0].size(),false));
         int res=0,n=grid.size(),m=grid[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                res=max(res,solve(i,j,n,m,grid,vis));
+                res=max(res,solve(i,j,n,m,grid));
             }
         }
         return res;
