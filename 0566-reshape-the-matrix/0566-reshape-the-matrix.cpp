96c4f41aae851fe9da961362a4055a8450216acf
samantro <66985251+samantro@users.noreply.github.com>
2023-02-07 19:31:03 +0530

    Time: 14 ms (45.00%), Space: 10.5 MB (99.62%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -3,13 +3,16 @@ public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
         int n=mat.size(),m=mat[0].size();
         if(m*n!=r*c) return mat;
-        for(int i=1;i<mat.size();i++){
-            mat[0].insert(mat[0].end(),mat[i].begin(),mat[i].end());
-        }
+        int k=0,l=0;
         vector<vector<int>> v(r,vector<int>(c));
-        int i=0;
-        for(int j=0;j<r;j++){
-            for(int k=0;k<c;k++) v[j][k]=mat[0][i++];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                v[k][l++]=mat[i][j];
+                if(l==c){
+                    l=0;
+                    k++;
+                }
+            }
         }
         return v;
     }
