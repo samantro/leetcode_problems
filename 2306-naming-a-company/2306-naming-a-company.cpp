5b3293ac5aaead5d9f23f1e9b56353b340edb046
samantro <66985251+samantro@users.noreply.github.com>
2023-02-09 14:06:23 +0530

    Time: 1369 ms (24.39%), Space: 85.8 MB (24.39%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.cpp
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -1,25 +1,21 @@
 class Solution {
     #include <string.h>
 public:
-    long long distinctNames(vector<string>& ideas) {
-        std::array<std::unordered_set<std::string>, 26> begCharToSuffix;
-        long long returnVal = 0;
-        for (int i = 0; i < ideas.size(); i++){
-            begCharToSuffix[ideas[i][0] - 'a'].emplace(ideas[i].substr(1));
-        }
-        for (int i = 0; i < 25; i++){
-            for (int j = i + 1; j < 26; j++){
-                long long mutualChars = 0;
-
-                for (auto suffix : begCharToSuffix[j]){
-                    if (begCharToSuffix[i].count(suffix)){
-                    ++mutualChars;
-                    }
+    long long distinctNames(vector<string>& id) {
+        int n=id.size();
+        vector<set<string>> v(26);
+        long long res=0;
+        for(int i=0;i<n;i++) v[id[i][0]-'a'].insert(id[i].substr(1));
+        for(auto i=0;i<25;i++){
+            for(int j=i+1;j<26;j++){
+                int ans=0;
+                for(auto k:v[j]){
+                    if(v[i].find(k)!=v[i].end()) ans++;
                 }
-                returnVal += 2LL * (begCharToSuffix[i].size() - mutualChars) * (begCharToSuffix[j].size() - mutualChars);
+                res+= 2LL*((v[i].size()-ans)*(v[j].size()-ans));
             }
         }
-
-        return returnVal;
+        return res;
+        
     }
 };
