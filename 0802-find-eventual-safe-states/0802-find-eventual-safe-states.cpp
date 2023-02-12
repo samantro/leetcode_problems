8649bf8598e4506cf5739913d0db3b206ce1d4a0
samantro <66985251+samantro@users.noreply.github.com>
2023-02-12 22:13:13 +0530

    Time: 248 ms (22.93%), Space: 60.7 MB (25.35%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+  bool dfs(int node,vector<int> &path,vector<int> &v,vector<int> &check,vector<int> adj[])
+  {
+      v[node]=1;
+      path[node]=1;
+      for(auto i:adj[node])
+      {
+          if(v[i]==0 && path[i]==0)
+          {
+              if(dfs(i,path,v,check,adj)==true)
+              {
+                  return true;
+              }
+          }
+          else if(v[i]==1 && path[i]==1)
+              {
+                  return true;
+              }
+      }
+      path[node]=0;
+      check[node]=1;
+      return false;
+  }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        // first of all creating a adjlist
+        int V=graph.size(); // total number of rows present in graph
+        vector<int> adj[V];
+        for(int i=0;i<graph.size();i++){
+            for(int j=0;j<graph[i].size();j++){
+                adj[i].push_back(graph[i][j]);
+            }
+        }
+        vector<int> v(V,0);
+        vector<int> path(V,0);
+        vector<int> check(V,0);
+        for(int i=0;i<V;i++)
+        {
+            if(v[i]==0)
+            {
+                dfs(i,path,v,check,adj);
+            }
+        }
+        vector<int> ans;
+        for(int i=0;i<V;i++)
+        {
+            if(check[i]==1)
+            {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
