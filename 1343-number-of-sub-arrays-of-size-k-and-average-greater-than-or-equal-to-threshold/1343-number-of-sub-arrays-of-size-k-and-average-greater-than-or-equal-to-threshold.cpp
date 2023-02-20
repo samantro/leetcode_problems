fa520adb3d418013e18cee8cde8c42ed7e7e39b7
samantro <66985251+samantro@users.noreply.github.com>
2023-02-20 13:35:22 +0530

    Time: 100 ms (45.89%), Space: 55.3 MB (74.84%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -4,11 +4,10 @@ public:
         int res=0,sum=0; 
         for(int i=0;i<arr.size();i++){
             sum += arr[i];
-            if(i==k-1 && sum/k>=threshold) res++;
-            else if(i>=k){
+            if(i>=k){
                 sum-=arr[i-k];
                 if(sum/k>=threshold) res++;
-            }
+            }else if(i==k-1 && sum/k>=threshold) res++;
         }
         return res;
     }
