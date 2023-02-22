cebddbd25c4166446c763ed6f6c228639a572059
samantro <66985251+samantro@users.noreply.github.com>
2023-02-22 17:40:19 +0530

    Time: 85 ms (77.30%), Space: 45.7 MB (16.22%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} weights
+ * @param {number} days
+ * @return {number}
+ */
+var shipWithinDays = function(weights, days) {
+    let left = Math.max(...weights);
+    let right = weights.reduce((sum, weight) => sum + weight, 0);
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (isPossible(weights, days, mid)) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+}
+
+const isPossible = (weights, days, capacity) => {
+    let daysNeeded = 1;
+    let currentLoad = 0;
+    for (let weight of weights) {
+        if (currentLoad + weight > capacity) {
+            currentLoad = 0;
+            daysNeeded += 1;
+        }
+        currentLoad += weight;
+    }
+    return daysNeeded <= days;
+}
