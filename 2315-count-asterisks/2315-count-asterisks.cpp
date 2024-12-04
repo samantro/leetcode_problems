8f1871e58b0b61cb6da4cb32a86f06029404e293
samantro <66985251+samantro@users.noreply.github.com>
2024-12-04 20:03:09 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (15.73%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.cpp
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int countAsterisks(string s) {
-        int b = 0, a = 0, a1 = 0;
+        int b = 0, a = 0;
         for(int i = 0; i< s.size();i++){
             if(s[i]=='*' && b == 0){
                 a++;
             }else if(s[i]=='|'){
                 b++;
-                if(s[i]=='*') a1++;
-                if(b==2){
-                    b = 0;
-                    a1 = 0;
-                }
+                if(b==2) b = 0;
             }
         }
-        return a + a1;
+        return a;
     }
 };
